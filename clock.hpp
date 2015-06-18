156a5aeb1f951539fc948826eb96573c7e14981b
BruceJohnJennerLawso <johnnybgoode@rogers.com>
2015-06-18 14:42:09 -0400

    the classes are really coming together, looks like a smooth interface, so long as I can use std::vector

--- clock.hpp
+++ clock.hpp
@@ -15,18 +15,50 @@
 #define ClOcK
 
 #include "Arduino.h"
+#include <vector>
+
+typedef double time;
+typedef short int score;
+
+enum player {home, away};
 
 class gameClock
 {
-	gameClock(int init_time, int period_minutes);
-	
-	float Time;
+	public:
+	gameClock(time init_time, int period_minutes);
 
-	void Update();
-	// ahh, more params
+	private:
+	time Time;
+
+	public:
+	void Update(time deltat);
+	time getClockTime();
+	bool periodOver();
+	void stopClock();
+	// generic one, regardless of what event triggered the clock to stop
+	void startClock();
+	String getClockOutput();
+	String getClockOutput(time deltat);
 
 	~gameClock();
+};
 
+class Game
+{	public:
+	
+	Game(int period_minutes);
+	
+	private:
+	std::vector<gameClock> Periods;
+	// dont even know if vector is gonna work here, we'll see
+	// its not a problem for regular games, but we can have arbitrary
+	// number of overtimes, which complicates things
+
+	score homeScore, awayScore;
+
+	public:
+	void Update(time deltat);
+	void Goal(player scoredBy);
 
 };
 
